8fa703ac65342b234893a8fe229dc07ad3890b4d
raycure <raycure666@gmail.com>
2024-08-11 15:46:53 +0300

    im being forced to

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,41 +3,51 @@ import './Navbar.css';
 import { Link } from 'react-router-dom';
 import { FaUser } from 'react-icons/fa6';
 import Button from '../Button/Button';
+import logo from '../../assets/LesmillsLogo.png';
 function Navbar() {
 	return (
 		<>
 			<nav className='nav-container'>
-				<ul>
-					<li>
-						<Link to='' className='nav-item'>
+				<Link aria-label='logo'>
+					<img alt='beep' className='img' src={logo}></img>
+				</Link>
+				<ul className='nav-item-container'>
+					<li className='nav-item'>
+						<Link to='/main' className='nav-item'>
 							Ana Sayfa
 						</Link>
 					</li>
-					<li>
-						<Link to='' className='nav-item'>
+					<li className='nav-item'>
+						<Link to='/classes' className='nav-item'>
 							Kurslar
 						</Link>
 					</li>
-					<li>
-						<Link to='' className='nav-item'>
+					<li className='nav-item'>
+						<Link to='/news' className='nav-item'>
 							Duyurular
 						</Link>
 					</li>
-					<li>
-						<Link to='' className='nav-item'>
+					<li className='nav-item'>
+						<Link to='/myCourses' className='nav-item'>
 							Benim Kurslarım
 						</Link>
 					</li>
-					<li>
-						<Link to='' className='nav-item'>
+					<li className='nav-item'>
+						<Link to='/contact' className='nav-item'>
 							İletişim
 						</Link>
 					</li>
 				</ul>
-				<Link to='' className='nav-item-icon'>
-					<FaUser />
-				</Link>
-				<Button className='nav-button'>Kaydol</Button>
+				<div className=' nav-item-container'>
+					<Link
+						to='userInfo'
+						className='nav-item '
+						aria-label='user Info or login'
+					>
+						<FaUser className='nav-item-icon' />
+					</Link>
+					<Button className='nav-button'>Kaydol</Button>
+				</div>
 			</nav>
 		</>
 	);
