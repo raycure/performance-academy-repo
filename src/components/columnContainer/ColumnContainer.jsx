b636abc8ac47dcf88f24f871b5e7fd1eb40f9a99
devemresr <dev.emresr@gmail.com>
2024-08-11 12:36:15 +0300

    kill delete

---
+++ src/components/columnContainer/ColumnContainer.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./columnStyle.css";
+function ColumnContainer({ children }) {
+  return( <div className="container"> {children}</div>)
+}
+export default ColumnContainer;
--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -3,13 +3,16 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
 import Button from '../../components/Button/Button';
+import ColumnContainer from '../../components/columnContainer/columnContainer';
 function Layout() {
 	return (
 		<>
 			<Navbar />
+<ColumnContainer>gasjf</ColumnContainer>
 			<Outlet />
 			<Footer />
 		</>
+		
 	);
 }
 export default Layout;
