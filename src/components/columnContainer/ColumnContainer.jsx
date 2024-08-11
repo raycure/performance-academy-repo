8c55a8ac68ebc4c1a80b4a4e4b6398fb13fd56e9
devemresr <dev.emresr@gmail.com>
2024-08-11 15:45:53 +0300

    meg

--- src/components/columnContainer/ColumnContainer.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./columnStyle.css";
-function ColumnContainer({ children }) {
-  return( <div className="container"> {children}</div>)
-}
-export default ColumnContainer;
--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from '../../components/Navbar/Navbar';
-import Footer from '../../components/Footer/Footer';
-import Button from '../../components/Button/Button';
-import ColumnContainer from '../../components/columnContainer/columnContainer';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+import Button from "../../components/Button/Button";
+
 function Layout() {
-	return (
-		<>
-			<Navbar />
-<ColumnContainer>gasjf</ColumnContainer>
-			<Outlet />
-			<Footer />
-		</>
-		
-	);
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
 }
 export default Layout;
