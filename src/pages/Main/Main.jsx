41bf97390891c076303504a9f0f938d4ffb5fd3d
devemresr <dev.emresr@gmail.com>
2024-08-13 15:40:15 +0300

    containers vid

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import "./Main.css";
 import Container from "../../components/Containers/Container";
+import isim from "../../assets/ornek.jpg"
+import Card from "../../components/Cards/Card";
 function Main() {
+  
   return (
     <>
-      <Container className="even-columns">
-        <div className="card-container">
-          <div className="card">
-            <div className="front">Front</div>
-            <div className="back">Back</div>
-          </div>
-        </div>
-        <div className="absolute"> vodka</div>
+      <Container style={{gap: '0'}} className="even-columns" >
+      <Card>
+      </Card>
+      <Card>
+      </Card>
+      <Card>
+      </Card>
       </Container>
 
       <Container>
@@ -20,7 +22,16 @@ function Main() {
           Your browser does not support the video tag.
         </video>
       </Container>
-      <Container>
+
+      <Container className="even-columns">
+        <div>
+        <div className="fs-primary-heading">ornek title</div>
+        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+        </div>
+        <img src={isim} className="image">
+        </img>
+
+
 
       </Container>
     </>
