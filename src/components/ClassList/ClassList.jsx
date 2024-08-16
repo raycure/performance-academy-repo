9ff27c5d2acf55aed1806782a250bef2ffe06d52
raycure <raycure666@gmail.com>
2024-08-16 17:30:20 +0300

    meow

--- src/components/ClassList/ClassList.jsx
+++ src/components/ClassList/ClassList.jsx
@@ -1,48 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './ClassList.css';
 import Button from '../Button/Button';
 import { lesMillsPrograms } from '../../assets/LesmillsPrograms';
 import { MdOutlineDoubleArrow } from 'react-icons/md';
 import name from '../../assets/ornek.jpg';
+import BODYPUMP from '../../assets/classLogo/BODYPUMP.png';
+
 function ClassList({ classType }) {
-	function classClickHandler() {}
+	//useEffect(() => {}, []);
+	//function classClickHandler() {}
 	const classes = Object.keys(lesMillsPrograms).map((category) => {
 		if (category !== classType && classType !== 'all') {
 			return;
 		}
 		return (
 			<>
-				{lesMillsPrograms[category].map((program, subIndex) => (
-					<div
-						className='class-item-container top-border-light row'
-						onClick={classClickHandler}
-					>
-						<img
-							aria-label='program pic'
-							className='img class-img'
-							src={name}
-						/>
-						<div className='info-container'>
+				{lesMillsPrograms[category].map((program, subIndex) => {
+					return (
+						<div
+							className='class-item-container class-text-container top-border-light row'
+							//onClick={classClickHandler}
+						>
 							<img
-								aria-label='logo'
-								className='img logo'
+								aria-label='program pic'
+								className='img class-img'
 								src={name}
 							/>
-							<h2 className='slogan'>{program.title}</h2>
-							<div className='row more-button-container top-border-light'>
-								<p>
-									Egzersiz tipi
-									<br />
-									{program.type}
-								</p>
-								<Button>
-									Daha Fazlası
-									<MdOutlineDoubleArrow color='white' />
-								</Button>
+							<div className='info-container'>
+								<div>
+									<img
+										aria-label='logo'
+										className='img class-logo'
+										src={BODYPUMP}
+									/>
+									<p className='slogan'>{program.sum}</p>
+								</div>
+								<div className='row more-button-container top-border-light'>
+									<div>
+										<p>Egzersiz Tipi: {program.type}</p>
+										<p>Ekipman: {program.equipment}</p>
+										<p>Kime Yönelik: {program.for}</p>
+									</div>
+									<Button>
+										Daha Fazlası
+										<MdOutlineDoubleArrow color='white' />
+									</Button>
+								</div>
 							</div>
 						</div>
-					</div>
-				))}
+					);
+				})}
 			</>
 		);
 	});
