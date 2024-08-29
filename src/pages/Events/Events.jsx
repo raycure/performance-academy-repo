3207ebae720b5465b11b17c3c6c7fb375b3c4c3c
raycure <raycure666@gmail.com>
2024-08-29 23:00:35 +0300

    event and register responsive

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -5,7 +5,7 @@ import CardCarousel from '../../components/Carousels/CardCarousel';
 import EventExpandedItem from '../../components/EventItem/EventExpandedItem';
 import PaginationContainer from '../../components/Containers/PaginationContainer';
 import CalendarEventItem from '../../components/CalendarContainer/CalendarEventItem';
-
+import backgroundText from '../../assets/CHOOSE-HAPPY.png';
 function Events() {
 	return (
 		<>
@@ -15,6 +15,12 @@ function Events() {
 					alt='events page hero'
 					className='background-image'
 				/>
+				<img
+					src={backgroundText}
+					alt='events page hero'
+					className='events-hero-text'
+				/>
+
 				<p className='text-container'></p>
 			</div>
 			<CalendarContainer />
