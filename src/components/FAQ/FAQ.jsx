15dd52b118dd5d79dd4e3846a73800d0e737ed9a
raycure <raycure666@gmail.com>
2024-08-29 13:32:07 +0300

    pulling

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -1,64 +1,70 @@
-import React, { useState } from "react";
-import FAQItem from "./FAQItem";
-import { faqQuestions } from "../../assets/FAQQuestions";
-import "./FAQ.css";
-import { motion } from "framer-motion";
+import React, { useState } from 'react';
+import FAQItem from './FAQItem';
+import { faqQuestions } from '../../assets/FAQQuestions';
+import './FAQ.css';
+import { motion } from 'framer-motion';
 import {
-  leftToRight,
-  ScalingAnimations,
-} from "../animations/AnimationValues.jsx";
+	leftToRight,
+	ScalingAnimations,
+} from '../animations/AnimationValues.jsx';
 
 function FAQ() {
-  const [activeQuestionTitle, setActiveQuestionTitle] = useState(null);
-  const activeResponse = faqQuestions
-    .filter((item) => {
-      return item.title === activeQuestionTitle;
-    })
-    .map((item) => {
-      return (
-        <motion.div
-          variants={ScalingAnimations}
-          initial="hidden"
-          animate="getBigger"
-          key={activeQuestionTitle}
-        >
-          {" "}
-          <div className="fw-regular fs-500">{item.response}</div>;
-        </motion.div>
-      );
-    });
-  return (
-    <div className="faq-outer-container text-container">
-      <div>
-        {faqQuestions.map((item, index) => {
-          return (
-            <motion.div
-              key={index}
-              variants={leftToRight}
-              initial="hidden"
-              whileInView="show"
-              viewport={{ once: true }}
-              custom={index}
-            >
-              <div
-                className={`${
-                  activeQuestionTitle === item.title ? "faq-item-active" : ""
-                }`}
-                onClick={() => setActiveQuestionTitle(item.title)}
-              >
-                <FAQItem>{item.title}</FAQItem>
-              </div>
-            </motion.div>
-          );
-        })}
-      </div>
-      <div>
-        {activeQuestionTitle === null && (
-          <p className="fs-primary-heading">Merak ettiğiniz bir şey mi var?</p>
-        )}
-        {activeResponse}
-      </div>
-    </div>
-  );
+	const [activeQuestionTitle, setActiveQuestionTitle] = useState(null);
+	const activeResponse = faqQuestions
+		.filter((item) => {
+			return item.title === activeQuestionTitle;
+		})
+		.map((item) => {
+			return (
+				<motion.div
+					variants={ScalingAnimations}
+					initial='hidden'
+					animate='getBigger'
+					key={activeQuestionTitle}
+				>
+					{' '}
+					<div className='fw-regular fs-500'>{item.response}</div>
+				</motion.div>
+			);
+		});
+	return (
+		<div className='faq-outer-container text-container'>
+			<div>
+				{faqQuestions.map((item, index) => {
+					return (
+						<motion.div
+							key={index}
+							variants={leftToRight}
+							initial='hidden'
+							whileInView='show'
+							viewport={{ once: true }}
+							custom={index}
+						>
+							<div
+								className={`${
+									activeQuestionTitle === item.title
+										? 'faq-item-active'
+										: ''
+								}`}
+								onClick={() =>
+									setActiveQuestionTitle(item.title)
+								}
+							>
+								<FAQItem>{item.title}</FAQItem>
+							</div>
+						</motion.div>
+					);
+				})}
+			</div>
+			<div>
+				{activeQuestionTitle === null && (
+					<p className='fs-primary-heading'>
+						Merak ettiğiniz bir şey mi var?
+					</p>
+				)}
+				{activeResponse}
+			</div>
+		</div>
+	);
 }
 export default FAQ;
