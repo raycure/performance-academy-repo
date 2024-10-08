e40284c04753e6a35c2f6c424c682ae24bc9a5e2
devemresr <dev.emresr@gmail.com>
2024-10-08 20:02:53 +0300

    added a couple of changes for ux: logging the user after registering, (later to be changed) a way to track if user logged in, for test purposes added a logout and restricted path, blocked logged in user to access the log in page

--- backend/Api/Controllers/registerController.js
+++ backend/Api/Controllers/registerController.js
@@ -4,6 +4,9 @@ const { hash, compare } = pkg;
 import jwt from 'jsonwebtoken';
 import { ObjectId } from 'mongodb';
 import EmailSender from './emailSender.js';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import Sessions from '../Models/sessionModel.js';
 
 const register = async (req, res) => {
 	try {
@@ -11,7 +14,6 @@ const register = async (req, res) => {
 		const existingUser = await Users.findOne({ email });
 		if (existingUser) {
 			return res.status(409).json({
-				attempts: existingUser,
 				success: false,
 				result: null,
 				message: 'Email already in use.',
@@ -36,10 +38,38 @@ const register = async (req, res) => {
 		} catch (error) {
 			console.log('email couldnt been sent');
 		}
-
-		// existingUser.attemptsToLogin = 0;
-		// await existingUser.save();
-		res.sendStatus(200);
+		const accessToken = jwt.sign(
+			{
+				userId: userId,
+				email: email,
+			},
+			process.env.ACCESS_TOKEN_SECRET,
+			{ expiresIn: '3s' } //todo change it
+		);
+		const refreshToken = jwt.sign(
+			{
+				userId: userId,
+			},
+			process.env.REFRESH_TOKEN_SECRET,
+			{ expiresIn: '5m' } //todo change it
+		);
+		res.cookie('jwt', refreshToken, {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24 * 30,
+			// maxAge: 1000 * 3, //todo delete it
+			sameSite: 'Lax',
+			path: '/',
+			secure: process.env.ENVIROMENT === 'development' ? 'false' : 'true',
+		});
+		const addActiveUser = await Sessions.create({
+			token: refreshToken,
+			userId: userId,
+			// expiresAt: new Date(Date.now() + 30 * 1000), // 7 days from now
+		});
+		res.status(200).json({
+			accessToken: accessToken,
+			message: 'Successfully login user',
+		});
 
 		// todo response with the users id nothing else ig.
 	} catch (error) {
