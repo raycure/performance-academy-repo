8b91cac1b6c370b093b7bb389ea208b0d94bff80
devemresr <dev.emresr@gmail.com>
2024-12-30 22:13:11 +0300

    testing pdf upload

--- backend/Api/Controllers/uploadFile.js
+++ backend/Api/Controllers/uploadFile.js
@@ -25,11 +25,12 @@ const fileSchema = new mongoose.Schema(
 	}
 );
 
+const uploadDir = path.join(process.cwd(), 'uploads');
 const File = mongoose.model('File', fileSchema);
 
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, 'uploads/');
+		cb(null, uploadDir);
 	},
 	filename: function (req, file, cb) {
 		const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
